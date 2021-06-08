b8a2fadcdc64ef3d9c2c12eaa61d014e4df93886
Taliena <natalie_sevas@yahoo.com>
2021-06-08 22:35:14 +0300

    adding styles and second tasc

--- TestAssignment.js
+++ TestAssignment.js
@@ -1,17 +1,27 @@
-window.addEventListener('load', function() {
-	const tabs = document.querySelectorAll('.container-nav__tab');
-	const content = document.querySelectorAll('.content-tab');
-	for(let i=0; i<tabs.length; i++) {
-	    tabs[i].addEventListener('click', function(){
-	        for(let i=0; i<tabs.length; i++) {
-	            tabs[i].className = 'container-nav__tab';
+window.addEventListener('load', function () {
+    const tabs = document.querySelectorAll('.container-nav__tab');
+    const content = document.querySelectorAll('.content-tab');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', function () {
+            for (let i = 0; i < tabs.length; i++) {
+                tabs[i].className = 'container-nav__tab';
             }
-	        this.className = 'container-nav__tab active';
+            this.className = 'container-nav__tab active';
 
-            for(let i=0; i<content.length; i++) {
+            for (let i = 0; i < content.length; i++) {
                 content[i].className = 'content-tab';
             }
             document.getElementById(this.dataset.id).className = 'content-tab active';
         });
     }
 });
+
+//#2
+function parseUrl(str) {
+    let subStr = str.slice(str.indexOf('/') + '//'.length, str.indexOf('?'));
+    return {
+        hash: str.slice(str.indexOf('#')),
+        hostname: subStr.slice(0, subStr.indexOf('/')),
+        pathname: subStr.slice(subStr.indexOf('/'))
+    }
+}
